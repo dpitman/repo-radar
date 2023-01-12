82e4f0b138fa553bfe730a335ed18224045417ff
dpitman <github@dpitman.com>
2023-01-12 10:30:41 -0500

    truncated the path, removed home dir, added notes for new features

--- bash/repo-radar.sh
+++ bash/repo-radar.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set the directory to the current working directory
 dir=$(pwd)
+home=$HOME
+echo $home
 
 # Find all of the git repositories in the given directory
 repos=$(find "$dir" -type d -name ".git")
@@ -15,6 +17,7 @@ do
     # Get the path and repository name
     path=$(dirname "$repo")
     name=$(basename "$path")
+    trunc_path="~${path#$home}"
 
     # Change to the repository directory
     cd "$path"
@@ -26,6 +29,14 @@ do
     # Count the number of stashed changes
     stashed=$(git stash list | wc -l)
 
+    # Check local vs remote HEAD
+    #local_head=$(git rev-parse HEAD)
+    #remote_head=$(git ls-remote origin HEAD)
+    #echo $local_head
+    #echo $remote_head
+    # TODO: check if remote repos is set, if set check loacal and remote
+    # TODO: if local and remote are not identical report
+
     # Print the repository report
-    printf "%-50s %-10s %-10s %s\n" "$path" "$untracked" "$stashed" "$name"
+    printf "%-50s %-10s %-10s %s\n" "$trunc_path" "$untracked" "$stashed" "$name"
 done
